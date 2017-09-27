dad0a71f77efa1ab1e64fb1ee2ad2a349b2b79ac
iussit <shagaev1983@mail.ru>
2017-09-27 22:15:44 +0400

    without photo and spam

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.create_account = new System.Windows.Forms.Button();
+            this.change_ip = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // create_account
+            // 
+            this.create_account.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.create_account.Location = new System.Drawing.Point(153, 12);
+            this.create_account.Name = "create_account";
+            this.create_account.Size = new System.Drawing.Size(111, 50);
+            this.create_account.TabIndex = 0;
+            this.create_account.Text = "Наебашить аков";
+            this.create_account.UseVisualStyleBackColor = false;
+            this.create_account.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // change_ip
+            // 
+            this.change_ip.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.change_ip.Location = new System.Drawing.Point(12, 12);
+            this.change_ip.Name = "change_ip";
+            this.change_ip.Size = new System.Drawing.Size(113, 50);
+            this.change_ip.TabIndex = 1;
+            this.change_ip.Text = "Выбор файла с прокси";
+            this.change_ip.UseVisualStyleBackColor = false;
+            this.change_ip.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(270, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(465, 182);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(747, 383);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.change_ip);
+            this.Controls.Add(this.create_account);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button create_account;
+        private System.Windows.Forms.Button change_ip;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using System.IO;
+using System.Net;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public List<String> ip = new List<String>();
+        IWebDriver Browser;
+        string salt = "money";
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            foreach (var pr in ip)
+            {
+                for (; ; )
+                {
+                    try
+                    {
+                        OpenQA.Selenium.Chrome.ChromeOptions options = new OpenQA.Selenium.Chrome.ChromeOptions();
+                        Proxy proxy = new Proxy();
+                        proxy.Kind = ProxyKind.Manual;
+                        proxy.IsAutoDetect = false;
+                        proxy.HttpProxy =
+                        proxy.SslProxy = pr.ToString();
+                        options.Proxy = proxy;
+                        options.AddArgument("ignore-certificate-errors");
+                        Browser = new OpenQA.Selenium.Chrome.ChromeDriver(options);
+
+                        Browser.Navigate().GoToUrl("https://www.pornhub.com/signup");
+
+                        try
+                        {
+                            IWebElement check_captcha = Browser.FindElement(By.ClassName("security"));
+                            Browser.Quit();
+                            break;
+                            //IWebElement captcha = Browser.FindElement(By.ClassName("recaptcha-checkbox-checkmark"));
+                            //captcha.Click();
+                            //auth(Browser);
+
+                            //break;
+                        }
+                        catch (Exception ex)
+                        {
+
+                            TimeSpan time;
+                            IWebElement email = Browser.FindElement(By.Id("email"));
+                            IWebElement password = Browser.FindElement(By.Id("password"));
+                            IWebElement login = Browser.FindElement(By.Id("username"));
+                            IWebElement submit = Browser.FindElement(By.Id("submit_buttom"));
+
+                            String randomchar = "";
+                            Random rand = new Random();
+                            for (int i = 0; i < 7; i++)
+                            {
+                                randomchar += (char)rand.Next(97, 122);
+                            }
+
+                            email.SendKeys(randomchar + "@mailertop.ru");
+                            password.SendKeys(randomchar + salt);
+                            login.SendKeys(randomchar);
+
+                            TimeSpan time1;
+                            DateTime initial_time = DateTime.Now;
+                            for (; ; )
+                            {
+                                DateTime current_time = DateTime.Now;
+                                time1 = current_time - initial_time;
+                                //textBox1.Text = time1.Seconds.ToString();
+                                if (time1.Seconds > 3) break;
+                            }
+
+
+                            submit.Click();
+
+                            DateTime initial_time5 = DateTime.Now;
+                            for (; ; )
+                            {
+                                DateTime current_time = DateTime.Now;
+                                time1 = current_time - initial_time5;
+                                //textBox1.Text = time1.Seconds.ToString();
+                                if (time1.Seconds > 3) break;
+                            }
+
+
+                            Browser.Navigate().GoToUrl("https://web12.beget.email/?_task=mail&_mbox=INBOX");
+                            IWebElement name = Browser.FindElement(By.Id("rcmloginuser"));
+                            IWebElement pas = Browser.FindElement(By.Id("rcmloginpwd"));
+                            IWebElement sub = Browser.FindElement(By.Id("rcmloginsubmit"));
+                            name.SendKeys("admin@mailertop.ru");
+                            pas.SendKeys("qwe123asd");
+                            sub.Click();
+
+                            IWebElement view = Browser.FindElement(By.Id("messagessearchfilter"));
+                            view.Click();
+
+
+                            List<IWebElement> options_message = Browser.FindElements(By.CssSelector("#messagessearchfilter option")).ToList();
+                            foreach (IWebElement op_ms in options_message)
+                            {
+                                if (op_ms.GetAttribute("value") == "UNSEEN")
+                                {
+                                    op_ms.Click();
+                                    break;
+                                }
+                            }
+
+
+                            DateTime initial = DateTime.Now;
+                            for (; ; )
+                            {
+                                DateTime current_time = DateTime.Now;
+                                time = current_time - initial;
+                                //textBox1.Text = time1.Seconds.ToString();
+                                if (time.Seconds > 6) break;
+                            }
+
+                            //IWebElement dont_read = view.FindElement(By.LinkText("Непрочитанные"));
+                            //dont_read.Click();
+
+
+
+                            IWebElement message = Browser.FindElement(By.ClassName("rcmContactAddress"));
+                            message.Click();
+                            message.Click();
+                            IWebElement check_url_mail = Browser.FindElement(By.ClassName("rcmBody"));
+                            IWebElement check_url_mail_atr = check_url_mail.FindElement(By.TagName("a"));
+                            check_url_mail_atr.Click();
+
+
+
+                            DateTime initial_time3 = DateTime.Now;
+                            for (; ; )
+                            {
+                                DateTime current_time = DateTime.Now;
+                                time = current_time - initial_time3;
+                                //textBox1.Text = time1.Seconds.ToString();
+                                if (time.Seconds > 3) break;
+                            }
+
+
+                            // fill profile
+                            IWebElement avatar = Browser.FindElement(By.ClassName("smallAvatar"));
+                            avatar.Click();
+                            IWebElement my_profile_sub = Browser.FindElement(By.ClassName("profile"));
+                            my_profile_sub.Click();
+                            IWebElement edit_profile = Browser.FindElement(By.ClassName("greyButton usersInteractionBtn"));
+                            edit_profile.Click();
+
+                            //profile photo
+                            //Random rand = new Random();
+                            string photo_name = "_" + rand.Next(1, 383);
+                            string photo_file = WindowsFormsApplication2.Properties.Resources.ResourceManager.GetObject(photo_name).ToString();
+
+                            IWebElement edit_profile_picture = Browser.FindElement(By.ClassName("spriteProfileEdit profilePictures"));
+                            edit_profile_picture.Click();
+                            IWebElement edit_profile_picture_button = Browser.FindElement(By.ClassName("spriteProfileIcons orangeButton float-left editProfileBtn editSprite avatarModalTrigger"));
+                            edit_profile_picture_button.Click();
+                            IWebElement input_picture = Browser.FindElement(By.ClassName("avtrUploadFile"));
+                            input_picture.SendKeys(photo_file);
+
+                        }
+
+                        Browser.Quit();
+                    }
+                    catch (Exception ex)
+                    {
+                        Browser.Quit();
+                        break;
+                    }
+                }
+            }
+
+
+        }
+
+        public void auth(IWebDriver Browser)
+        {
+
+            IWebElement email = Browser.FindElement(By.Id("email"));
+            IWebElement password = Browser.FindElement(By.Id("password"));
+            IWebElement login = Browser.FindElement(By.Id("username"));
+            IWebElement submit = Browser.FindElement(By.Id("submit_buttom"));
+
+            String randomchar = "";
+            Random rand = new Random();
+            for (int i = 0; i < 7; i++)
+            {
+                randomchar += (char)rand.Next(97, 122);
+            }
+
+            email.SendKeys(randomchar + "@mailertop.ru");
+            password.SendKeys(randomchar + salt);
+            login.SendKeys(randomchar);
+
+            TimeSpan time1;
+            DateTime initial_time = DateTime.Now;
+            for (; ; )
+            {
+                DateTime current_time = DateTime.Now;
+                time1 = current_time - initial_time;
+                //textBox1.Text = time1.Seconds.ToString();
+                if (time1.Seconds > 3) break;
+            }
+            submit.Click();
+
+            TimeSpan time;
+            DateTime initial_time1 = DateTime.Now;
+            for (; ; )
+            {
+                DateTime current_time = DateTime.Now;
+                time = current_time - initial_time1;
+                //textBox1.Text = time1.Seconds.ToString();
+                if (time.Seconds > 4) break;
+            }
+
+            //check mail
+            Browser.Navigate().GoToUrl("https://web12.beget.email/?_task=mail&_mbox=INBOX");
+            IWebElement name = Browser.FindElement(By.Id("rcmloginuser"));
+            IWebElement pas = Browser.FindElement(By.Id("rcmloginpwd"));
+            IWebElement sub = Browser.FindElement(By.Id("rcmloginsubmit"));
+            name.SendKeys("admin@mailertop.ru");
+            pas.SendKeys("qwe123asd");
+            sub.Click();
+            IWebElement message = Browser.FindElement(By.ClassName("rcmContactAddress"));
+            message.Click();
+            message.Click();
+            IWebElement check_url_mail = Browser.FindElement(By.ClassName("rcmBody"));
+            IWebElement check_url_mail_atr = check_url_mail.FindElement(By.TagName("a"));
+            check_url_mail_atr.Click();
+            DateTime initial_time2 = DateTime.Now;
+            for (; ; )
+            {
+                DateTime current_time = DateTime.Now;
+                time = current_time - initial_time2;
+                //textBox1.Text = time1.Seconds.ToString();
+                if (time.Seconds > 40) break;
+            }
+            //Browser.Quit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = "c:\\";
+
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                String path = openFileDialog1.FileName;
+                //textBox1.Text=path;
+                StreamReader file = new StreamReader(path);
+
+                while (!file.EndOfStream)
+                {
+                    string line = file.ReadLine();
+                    //user.Add(user.);
+                    ip.Add(line);
+                    //textBox1.Text += ip;
+                }
+                file.Close();
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+
+
+
+    }
+}
